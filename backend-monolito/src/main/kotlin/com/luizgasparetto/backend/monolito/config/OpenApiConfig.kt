a3ac88a8b208fbfce4202cc5a4bd1b94122e13eb
lmgaspa <luhmgasparetto@gmail.com>
2025-08-10 05:36:50 -0300

    swagger

--- backend-monolito/src/main/kotlin/com/luizgasparetto/backend/monolito/config/OpenApiConfig.kt
+++ backend-monolito/src/main/kotlin/com/luizgasparetto/backend/monolito/config/OpenApiConfig.kt
@@ -2,6 +2,7 @@ package com.luizgasparetto.backend.monolito.config
 
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -15,4 +16,5 @@ class OpenApiConfig {
                 .version("v1")
                 .description("Endpoints do checkout Pix, webhook e catálogo")
         )
+            .servers(listOf(Server().url("https://ecommerceag-6fa0e6a5edbf.herokuapp.com")))
 }
