13b06336d6afad7ac82910c7a736efd5d4729863
lmgaspa <luhmgasparetto@gmail.com>
2025-09-18 01:08:53 -0300

    backend

--- backend-monolito/src/main/kotlin/com/luizgasparetto/backend/monolito/config/OpenApiConfig.kt
+++ backend-monolito/src/main/kotlin/com/luizgasparetto/backend/monolito/config/OpenApiConfig.kt
@@ -1,4 +1,4 @@
-package com.luizgasparetto.backend.monolito.config.swagger
+package com.luizgasparetto.backend.monolito.config
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
